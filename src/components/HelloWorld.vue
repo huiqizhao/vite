4a93c8b045cede42d439e6a5a2835ba37e782c6d
zhaohuiqi <huiqi.zhao@outlook.com>
2023-02-09 10:25:01 +0800

    scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,8 +31,8 @@ const number = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .read-the-docs {
-  color: #888;
+  color: $variable-red-color;
 }
 </style>
