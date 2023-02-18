9192f0d8aef20baac0fcb04a3921d0a7ad9ecf09
zhaohuiqi <huiqi.zhao@outlook.com>
2023-02-18 16:14:45 +0800

    mathjax

--- src/components/FormulaConatiner.vue
+++ src/components/FormulaConatiner.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
-const formula = ref('$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$')
+const formula = ref('x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}')
 </script>
 
 <style lang="scss" scoped>
